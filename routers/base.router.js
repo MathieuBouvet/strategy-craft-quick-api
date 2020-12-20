b18ed78f07f03691019d8d8ef7759777d5ab933c
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-12-20 17:28:31 +0100

    Register the production router

--- routers/base.router.js
+++ routers/base.router.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const baseController = require("../controllers/base.controller");
+const productionRouter = require("../routers/production.router");
 
 router.get("/:id", baseController.getBase);
+router.use("/:baseId/productions", productionRouter);
 
 module.exports = router;
