3fa165fc3250bcd7d88cb09a25826ee617831fe1
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-12-20 14:50:43 +0100

    Seed db

--- services/db.js
+++ services/db.js
@@ -1,9 +1,31 @@
 const lowdb = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
+const { createBase, createPlayer } = require("../utils/dbDefaults");
 
 const adapter = new FileSync("./data/db.json");
 const db = lowdb(adapter);
 
-db.defaults({ bases: [], players: [] }).write();
+const player1 = createPlayer(
+  0,
+  "player-one",
+  "player-one@test.com",
+  "password"
+);
+const base1 = createBase(0, player1);
+
+const player2 = createPlayer(
+  1,
+  "player-two",
+  "player-two@test.com",
+  "passwordForPlayer2"
+);
+
+const base2 = createBase(1, player2);
+const base3 = createBase(2, player2);
+
+db.defaults({
+  bases: [base1, base2, base3],
+  players: [player1, player2],
+}).write();
 
 module.exports = db;
