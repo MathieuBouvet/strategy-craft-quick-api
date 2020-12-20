61064b35c8d7bfd4ed660d767333cb34f7e853be
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-12-20 15:45:27 +0100

    Add the name of the Http error

--- utils/HttpErrors.js
+++ utils/HttpErrors.js
@@ -1,7 +1,16 @@
+const httpErrorCodes = {
+  400: "Bad Request",
+  401: "Unauthorized",
+  403: "Forbidden",
+  404: "Not Found",
+  409: "Conflict",
+};
+
 class HttpError extends Error {
   constructor(code, message) {
     super(message);
     this.code = code;
+    this.name = httpErrorCodes[code] ?? "unknown";
   }
 }
 
