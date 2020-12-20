748ac57994f76d35a6476305c8109ed5b374e2fc
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-12-20 14:50:30 +0100

    Create helper functions to seed db

---
+++ utils/dbDefaults.js
@@ -0,0 +1,36 @@
+function createProduction() {
+  return {
+    workers: 0,
+    level: 1,
+    upgrading: false,
+  };
+}
+
+function createBase(id, owner) {
+  owner.bases.push(id);
+  return {
+    id,
+    owner: owner.id,
+    idleWorkers: 10,
+    wood: 0,
+    food: 0,
+    ore: 0,
+    productions: {
+      wood: createProduction(),
+      food: createProduction(),
+      ore: createProduction(),
+    },
+  };
+}
+
+function createPlayer(id, name, mail, password) {
+  return {
+    id,
+    name,
+    mail,
+    password,
+    bases: [],
+  };
+}
+
+module.exports = { createBase, createPlayer };
