29fdcb9ce5b14a0876d97b9d1d5534a2b9f4366e
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-12-21 13:11:39 +0100

    Create a service to get upgrade cost

--- services/production.service.js
+++ services/production.service.js
@@ -1,6 +1,8 @@
 const db = require("./db.service");
 const baseService = require("./base.service");
 const HttpError = require("../utils/HttpErrors");
+const { productionSettings } = require("../gameSettings");
+const objectMap = require("../utils/objectMap");
 
 function getProduction(base, ressourceName) {
   return base.get("productions").get(ressourceName);
@@ -18,8 +20,17 @@ function validateRessourceName(ressourceName) {
   return ressourceName;
 }
 
+function getNextLevelCost(production) {
+  const ressourceName = production.name;
+  const currentLevel = production.data.value().level;
+  const upgradeBaseCost = productionSettings[ressourceName].upgradeCosts;
+  const costFn = cost => currentLevel * cost;
+  return objectMap(upgradeBaseCost, costFn);
+}
+
 module.exports = {
   getProduction,
   getProductionByBaseId,
   validateRessourceName,
+  getNextLevelCost,
 };
