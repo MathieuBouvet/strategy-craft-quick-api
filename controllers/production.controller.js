8462f4b17be7eb1742ab37eeb5d99ee317a93b2c
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-12-24 13:36:23 +0100

    Respond with upgrade time

--- controllers/production.controller.js
+++ controllers/production.controller.js
@@ -1,9 +1,13 @@
-const { getNextLevelCost } = require("../services/production.service");
+const {
+  getNextLevelCost,
+  getUpgradeTime,
+} = require("../services/production.service");
 
 function getProduction(req, res) {
   res.json({
     name: req.production.name,
     nextLevelCost: getNextLevelCost(req.production),
+    upgradeTime: getUpgradeTime(req.production),
     ...req.production.data.value(),
   });
 }
