e7eccc0e10dc35255cdb69702659ecdf4df64051
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-12-24 16:11:47 +0100

    Use representation service

--- controllers/production.controller.js
+++ controllers/production.controller.js
@@ -1,20 +1,7 @@
-const {
-  getNextLevelRessourceCost,
-  getNextLevelWorkerCost,
-  getUpgradeTime,
-} = require("../services/production.service");
+const { getRepresentation } = require("../services/production.service");
 
 function getProduction(req, res) {
-  const nextLevelCost = {
-    ...getNextLevelRessourceCost(req.production),
-    workers: getNextLevelWorkerCost(req.production),
-  };
-  res.json({
-    name: req.production.name,
-    nextLevelCost,
-    upgradeTime: getUpgradeTime(req.production),
-    ...req.production.data.value(),
-  });
+  res.json(getRepresentation(req.production));
 }
 
 module.exports = { getProduction };
