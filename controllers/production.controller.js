56f83e868a4ebea79d4e290e75ddf8a4d6671a41
Mathieu Bouvet <mathieubouvet42@gmail.com>
2020-12-24 17:05:04 +0100

    Add controller for upgrading a production

--- controllers/production.controller.js
+++ controllers/production.controller.js
@@ -1,7 +1,53 @@
-const { getRepresentation } = require("../services/production.service");
+const {
+  getNextLevelRessourceCost,
+  getNextLevelWorkerCost,
+  getRepresentation,
+  isThereEnoughWorkersToUpgrade,
+  setPendingUpgrade,
+  getWorkersToRemoveFromIdlePool,
+} = require("../services/production.service");
+const {
+  isThereEnoughRessources,
+  removeFromStock,
+} = require("../services/base.service");
+const requireEnoughStock = require("../services/requireEnoughStock");
 
 function getProduction(req, res) {
   res.json(getRepresentation(req.production));
 }
 
-module.exports = { getProduction };
+function upgrade(req, res) {
+  const productionData = req.production.data.value();
+  if (productionData.pendingUpgrade != null) {
+    return res.status(204).json();
+  }
+  const ressourceCostToUpgrade = getNextLevelRessourceCost(req.production);
+  const workerCostToUpgrade = getNextLevelWorkerCost(req.production);
+  const enoughRessourcesAndWorkers = {
+    ...isThereEnoughRessources(req.base, ressourceCostToUpgrade),
+    workers: isThereEnoughWorkersToUpgrade(
+      req.base,
+      req.production,
+      workerCostToUpgrade
+    ),
+  };
+  requireEnoughStock(enoughRessourcesAndWorkers, 409);
+
+  const workersToRemoveFromIdlePool = getWorkersToRemoveFromIdlePool(
+    req.production,
+    workerCostToUpgrade
+  );
+
+  removeFromStock(req.base, {
+    ...ressourceCostToUpgrade,
+    idleWorkers: workersToRemoveFromIdlePool,
+  });
+
+  setPendingUpgrade(req.production, {
+    ...ressourceCostToUpgrade,
+    workers: workerCostToUpgrade,
+  });
+  res.json(getRepresentation(req.production));
+}
+
+module.exports = { getProduction, upgrade };
